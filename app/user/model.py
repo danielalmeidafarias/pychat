2162e3463a2da76a81aea1bb217193da86a88cea
danielalmeidafarias <danielalmeidafarias2002@gmail.com>
2024-08-08 13:28:48 -0300

    fix: removing auto_implement id from model(the id was the same for every user)

--- app/user/model.py
+++ app/user/model.py
@@ -7,7 +7,8 @@ from app.db import db
 
 
 class UserModel(db.Model):
-    def __init__(self, email, name, password):
+    def __init__(self, id, email, name, password):
+        self.id = id
         self.email = email
         self.name = name
         self.password = password
@@ -15,10 +16,21 @@ class UserModel(db.Model):
     __table_args__ = {'extend_existing': True}
 
     __tablename__ = 'user'
-    id: Mapped[str] = mapped_column(primary_key=True, default=str(uuid.uuid4()))
+    id: Mapped[str] = mapped_column(primary_key=True)
     name: Mapped[str] = mapped_column()
     email: Mapped[str] = mapped_column(unique=True)
     password: Mapped[bytes] = mapped_column()
 
+    friends: Mapped[str] = mapped_column(default='')
+    friendship_request: Mapped[str] = mapped_column(default='')
+    sent_friendship_request: Mapped[str] = mapped_column(default='')
+
     def __repr__(self) -> str:
-        return f"{{id:{self.id}, name:{self.name}}}"
+        return f"{{id:{self.id}, \
+        name:{self.name}, \
+        email: {self.email}, \
+        password: {self.password}, \
+        friends:{self.friends}, \
+        friendship_request: {self.friendship_request} \
+        sent_friendship_request: {self.friendship_request} \
+        }}"
