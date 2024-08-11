f37d796a8ecaa78174a9d03baf2bc0e441d6bf63
danielalmeidafarias <danielalmeidafarias2002@gmail.com>
2024-08-11 20:05:50 -0300

    feat: friendship request repository

--- app/friendship_request/repository.py
+++ app/friendship_request/repository.py
@@ -1,21 +1,97 @@
+import itertools
+
 from flask_sqlalchemy import SQLAlchemy
+from .model import FriendshipRequestModel
+from itertools import chain
 
 
-class Friendship_requestRepository():
+class FriendshipRequestRepository():
     def __init__(self, db: SQLAlchemy):
         self.db = db
 
-    def create(self, friendship_request_id: str):
-        pass
+    def create(self, id: str, sender_id: str, recipient_id: str):
+        friendship_request = FriendshipRequestModel(
+            id=id,
+            sender_id=sender_id,
+            recipient_id=recipient_id
+        )
 
-    def get(self, friendship_request_id: str):
-        pass
+        self.db.session.add(friendship_request)
+        self.db.session.commit()
 
-    def get_all(self):
-        pass
+        return friendship_request
 
-    def update(self, friendship_request_id: str):
-        pass
+    def get_one(self, sender_id: str, recipient_id: str):
+        friendship_request = (self.db.session.query(FriendshipRequestModel)
+                              .where(FriendshipRequestModel.sender_id == sender_id and
+                                     FriendshipRequestModel.recipient_id == recipient_id).one_or_none())
+
+        return friendship_request
+
+    def get_one_by_id(self, id):
+        data = self.db.session.execute(
+            self.db.select(FriendshipRequestModel).
+            where(FriendshipRequestModel.id == id)).scalar_one()
+
+        return {
+            "id": data.id,
+            "sender_id": data.sender_id,
+            "recipient_id": data.recipient_id,
+            "status": data.status
+        }
+
+    def get_sent(self, sender_id):
+        data = self.db.session.execute(
+            self.db.select(FriendshipRequestModel).
+            where(FriendshipRequestModel.sender_id == sender_id)).scalars()
+
+        friendship_requests = [
+            {
+                "id": request.id,
+                "sender_id": request.sender_id,
+                "recipient_id": request.recipient_id,
+                "status": request.status
+            }
+            for request in data
+        ]
+
+        return friendship_requests
+
+    def get_received(self, recipient_id):
+        data = self.db.session.execute(
+            self.db.select(FriendshipRequestModel).
+            where(FriendshipRequestModel.recipient_id == recipient_id)).scalars()
+
+        friendship_requests = [
+            {
+                "id": request.id,
+                "sender_id": request.sender_id,
+                "recipient_id": request.recipient_id,
+                "status": request.status
+            }
+            for request in data
+        ]
+
+        return friendship_requests
+
+    def get_all(self, user_id):
+        sent = self.get_sent(sender_id=user_id)
+        received = self.get_received(recipient_id=user_id)
+
+        total = []
+        for request in itertools.chain(sent, received):
+            total.append(request)
+
+        return total
+
+    def update(self, friendship_request_id: str, status, recipient_id: str or None):
+        (self.db.session.query(FriendshipRequestModel).where(FriendshipRequestModel.id == friendship_request_id)
+         .update({
+            "status": status,
+            "recipient_id": recipient_id
+         })
+         )
+        self.db.session.commit()
 
     def delete(self, friendship_request_id: str):
         pass
