e26ee2838c927636c46837bcd9d2d755b74da755
danielalmeidafarias <danielalmeidafarias2002@gmail.com>
2024-08-11 20:03:39 -0300

    feat: friendship request model

--- app/friendship_request/model.py
+++ app/friendship_request/model.py
@@ -1,18 +1,19 @@
-import uuid
 from sqlalchemy.orm import Mapped, mapped_column
 from app.db import db
 
 
-class Friendship_requestModel(db.Model):
-    def __init__(self):
-       pass
+class FriendshipRequestModel(db.Model):
+    def __init__(self, id, sender_id, recipient_id):
+        self.id = id
+        self.sender_id = sender_id
+        self.recipient_id = recipient_id
 
     __table_args__ = {'extend_existing': True}
 
-    __tablename__ = 'user'
-
     id: Mapped[str] = mapped_column(primary_key=True)
+    sender_id: Mapped[str] = mapped_column(nullable=False)
+    recipient_id: Mapped[str] = mapped_column(nullable=False)
+    status: Mapped[str] = mapped_column(default='open')
 
     def __repr__(self) -> str:
-        return f"{{id:{self.id}}}"
-
+        return f"{{id:{self.id}, sender_id: {self.sender_id}, recipient_id: {self.recipient_id}, status: {self.status}"
