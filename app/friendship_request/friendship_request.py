29620158b929ccf2325a640b8c229a23776cf3ef
danielalmeidafarias <danielalmeidafarias2002@gmail.com>
2024-08-11 20:02:41 -0300

    feat: friendship request route

--- app/friendship_request/friendship_request.py
+++ app/friendship_request/friendship_request.py
@@ -1,21 +1,27 @@
+import uuid
+
 from flask import request
 from flask_restx import Resource, Namespace
 from app.db import db
 from marshmallow.exceptions import ValidationError
-from .model import Friendship_requestModel
+from .model import FriendshipRequestModel
 from .schemas import GetFriendshipRequestSchema, CreateFriendshipRequestSchema, UpdateFriendshipRequestSchema, DeleteFriendshipRequestSchema
 from .docs.response_models import Friendship_requestResponseModels
 from .docs.request_models import Friendship_requestRequestModels
 from app.common.docs.response_models import CommonResponseModels
-from ..middlewares.auth_middleware import auth_middleware, decode_jwt
+from ..middlewares.auth_middleware import auth_middleware
 from ..user.user_repository import UserRepository
 from sqlalchemy.exc import NoResultFound, OperationalError
+from .repository import FriendshipRequestRepository
+from ..auth.functions.functions import AuthFunctions
 
 friendship_request_namespace = Namespace('friendship_request', 'Friendship_request Route')
 requests = Friendship_requestRequestModels(friendship_request_namespace)
 responses = Friendship_requestResponseModels(friendship_request_namespace)
 common_responses = CommonResponseModels(friendship_request_namespace)
 user_repository = UserRepository(db)
+friendship_request_repository = FriendshipRequestRepository(db)
+auth_functions = AuthFunctions()
 
 
 @auth_middleware
@@ -24,43 +30,56 @@ user_repository = UserRepository(db)
 @friendship_request_namespace.response(code=409, model=common_responses.unauthorized, description='Unauthorized')
 @friendship_request_namespace.route('')
 class Friendship_requestResource(Resource):
-    # Receber as solicitações
     def get(self):
-        pass
+        received = request.args.get('received')
+        sent = request.args.get('sent')
+        user_id = auth_functions.decode_jwt((request.headers.get('Authorization')))['user_id']
 
-    # Enviar uma solicitação
-    # Caso ja forem amigos ou ja foi enviado uma solicitação, não deve ser possivel enviar
+        if bool(received) and bool(sent) or not bool(received) and not bool(sent):
+            friendship_requests = friendship_request_repository.get_sent(user_id)
+            return friendship_requests
+        elif bool(received):
+            friendship_requests = friendship_request_repository.get_received(user_id)
+            return friendship_requests
+        else:
+            friendship_requests = friendship_request_repository.get_sent(user_id)
+            return friendship_requests
+
     def post(self):
-        data = request.get_json()
-        schema = CreateFriendshipRequestSchema()
-        validated_data = schema.dump(data)
+        recipient_id = request.get_json()['recipient_id']
 
+        sender_id = auth_functions.decode_jwt((request.headers.get('Authorization')))['user_id']
+
+        schema = CreateFriendshipRequestSchema()
+        validated_data = schema.dump({
+            "recipient_id": recipient_id,
+            "sender_id": sender_id,
+            "id": uuid.uuid4()
+        })
         try:
             schema.load(validated_data)
         except ValidationError as err:
             return {'message': 'Data Validation Error!', 'errors': err.messages}, 400
 
-        requesting_user_id = decode_jwt(request.headers.get('Authorization'))['user_id']
-
         try:
-            requesting_user = user_repository.get(requesting_user_id)
+            sender_user = user_repository.get(sender_id)
         except NoResultFound:
             return {
                 "message": "Unauthorized!"
             }, 401
 
         try:
-            recipient_user = user_repository.get(validated_data['recipient_id'])
+            recipient_user = user_repository.get(recipient_id)
         except NoResultFound:
             return {
                 "message": "No User Found"
             }, 404
 
         try:
-            user_friends_str: str = requesting_user['friends']
-            user_friends = user_friends_str.split(',')
+            sender_friends_str: str = sender_user['friends']
+            sender_friends = sender_friends_str.split(',')
 
-            already_friend_index = user_friends.index(recipient_user['id'])
+            already_friend_index = sender_friends.index(recipient_user['id'])
 
             if already_friend_index is not None:
                 return {
@@ -69,51 +88,69 @@ class Friendship_requestResource(Resource):
         except ValueError:
             pass
 
-        try:
-            user_friendship_requests_str = requesting_user['sent_friendship_request']
-            user_friendship_requests = user_friendship_requests_str.split(',')
-            friendship_request_already_sent_index = user_friendship_requests.index(recipient_user['id'])
+        friendship_already_requested = friendship_request_repository.get_one(sender_id=sender_id, recipient_id=recipient_id)
 
-            if friendship_request_already_sent_index is not None:
-                return {
-                    "message": 'You had already sent a friendship_request for this user'
-                }, 400
-        except ValueError:
-            pass
-
-        try:
-            user_repository.update(requesting_user_id, {'sent_friendship_request': validated_data['recipient_id']})
-            user_repository.update(validated_data['recipient_id'], {'friendship_request': requesting_user_id})
-
-            db.session.commit()
-        except OperationalError:
-            db.session.rollback()
+        if friendship_already_requested is not None:
             return {
-                "message": "Invalid data"
+                "message": 'You had already sent a friendship request for this user'
             }, 400
 
+        friendship_request = friendship_request_repository.create(sender_id=validated_data['sender_id'],
+                                                                  recipient_id=validated_data['recipient_id'],
+                                                                  id=validated_data['id'])
+
         return {
-            "message": "Friendship request sent"
+            "message": "Friendship request sent",
+            'friendship_request': {
+                "id": friendship_request.id,
+                "sender_id": friendship_request.sender_id,
+                "recipient_id": friendship_request.recipient_id,
+                "status": friendship_request.status
+            }
         }, 200
 
 
 @friendship_request_namespace.route('/<id>')
 class UniqueFriendship_requestResource(Resource):
-    # Aceitar ou recusar uma solicitação
-    # Nesse caso deve ser modificado dos dois usuarios envolvidos
-    def put(self):
+    def put(self, id):
         data = request.get_json()
-        schema = UpdateFriendship_requestSchema()
+        schema = UpdateFriendshipRequestSchema()
         validated_data = schema.dump(data)
+
+        user_id = auth_functions.decode_jwt(request.headers.get('Authorization'))['user_id']
 
         try:
             schema.load(validated_data)
         except ValidationError as err:
             return {'message': 'Data Validation Error!', 'errors': err.messages}, 400
-        pass
 
-    # Remover solicitações
-    # Deve recusar uma solicitação caso esta ainda esteja em aberto
-        # Nesse caso deve ser modificado dos dois usuarios envolvidos
+        try:
+            friendship_request = friendship_request_repository.get_one_by_id(id)
+        except NoResultFound:
+            return {
+                "message": "No friendship request with this id was found"
+            }, 404
+
+        if friendship_request['recipient_id'] != user_id:
+            return {
+                "message": "The user_id don't correspond to this friendship request's recipient_id"
+            }, 400
+
+        if validated_data['status'] == 'accepted':
+            friendship_request_repository.update(friendship_request['id'],
+                                                 status='accepted', recipient_id=user_id)
+
+            user_repository.add_friend(friendship_request['recipient_id'], friendship_request['sender_id'])
+            return {
+                "message": 'friendship request successfully accepted'
+            }, 200
+        elif validated_data['status'] == 'refused':
+            friendship_request_repository.update(friendship_request['id'],
+                                                 status='refused', recipient_id='')
+
+            return {
+                "message": 'friendship request successfully refused'
+            }
+
     def delete(self):
         pass
