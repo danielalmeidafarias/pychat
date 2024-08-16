f89671aa0083e7d4286db0bcfd345109dc78969e
danielalmeidafarias <danielalmeidafarias2002@gmail.com>
2024-08-16 00:21:34 -0300

    fix: moving auth functions to utils

--- app/auth/util.py
+++ app/auth/util.py
@@ -2,13 +2,17 @@ import jwt
 import os
 import datetime
 import bcrypt
+from flask import Response, make_response
+
 class AuthFunctions:
-    def decode_jwt(self, jwt_token: str):
+    @staticmethod
+    def decode_jwt(jwt_token: str):
         decoded_jwt = jwt.decode(str.encode(jwt_token), os.getenv('JWT_SECRET'), "HS256")
 
         return decoded_jwt
 
-    def get_access_token(self, user_id):
+    @staticmethod
+    def get_access_token(user_id):
         payload = {
             "user_id": str(user_id),
             "expires_at": str(datetime.datetime.now() + datetime.timedelta(hours=1))
@@ -21,10 +25,17 @@ class AuthFunctions:
 
         return access_token
 
-    def is_password_correct(self, password, hashed_password):
+    @staticmethod
+    def is_password_correct(password, hashed_password):
         is_password_correct = bcrypt.checkpw(
             password=str.encode(password),
             hashed_password=hashed_password
         )
 
         return is_password_correct
+
+    def set_auth_cookies(self, user_id: str, response: Response):
+        response.set_cookie('Authorization', self.get_access_token(user_id), httponly=True, secure=True,
+                            expires=(datetime.datetime.now() + datetime.timedelta(hours=1)))
+
+        return response
