08a2024979f6cbc437c4f87a7f7687d8e5bbe395
danielalmeidafarias <danielalmeidafarias2002@gmail.com>
2024-08-16 00:27:21 -0300

    fix: remodeling user and friendship for next new features

--- app/friendship_request/model.py
+++ app/friendship_request/model.py
@@ -1,19 +1,16 @@
-from sqlalchemy.orm import Mapped, mapped_column
 from app.db import db
+from datetime import datetime
 
-
-class FriendshipRequestModel(db.Model):
-    def __init__(self, id, sender_id, recipient_id):
-        self.id = id
-        self.sender_id = sender_id
-        self.recipient_id = recipient_id
-
-    __table_args__ = {'extend_existing': True}
-
-    id: Mapped[str] = mapped_column(primary_key=True)
-    sender_id: Mapped[str] = mapped_column(nullable=False)
-    recipient_id: Mapped[str] = mapped_column(nullable=False)
-    status: Mapped[str] = mapped_column(default='open')
+class FriendshipRequest(db.Model):
+    id = db.Column(db.String(36), primary_key=True)
+    sender_id = db.Column(db.String(36), db.ForeignKey('user.id'), nullable=False)
+    receiver_id = db.Column(db.String(36), db.ForeignKey('user.id'), nullable=False)
+    status = db.Column(db.String(20), nullable=False, default='pending')
+    created_at = db.Column(db.DateTime, default=datetime.now(), nullable=False)
 
     def __repr__(self) -> str:
-        return f"{{id:{self.id}, sender_id: {self.sender_id}, recipient_id: {self.recipient_id}, status: {self.status}"
+        return f"{{id:{self.id}, \
+        sender_id: {self.sender_id}, \
+        receiver_id: {self.receiver_id}, \
+        status: {self.status}, \
+        created_at: {self.created_at}"
--- app/user/model.py
+++ app/user/model.py
@@ -1,31 +1,43 @@
-import uuid
-
-from sqlalchemy.orm import Mapped, mapped_column
-from flask_sqlalchemy import SQLAlchemy
-from uuid import UUID as UUID4
 from app.db import db
 
+friendship = db.Table('friendship',
+                      db.Column('user_id', db.String, db.ForeignKey('user.id'), primary_key=True),
+                      db.Column('friend_id', db.String, db.ForeignKey('user.id'), primary_key=True)
+                      )
 
-class UserModel(db.Model):
-    def __init__(self, id, email, name, password):
-        self.id = id
-        self.email = email
+chat_members = db.Table('chat_members',
+    db.Column('user_id', db.Integer, db.ForeignKey('user.id'), primary_key=True),
+    db.Column('chat_id', db.Integer, db.ForeignKey('chat.id'), primary_key=True)
+)
+
+class User(db.Model):
+    def __init__(self, user_id: str, name: str, email: str, password: bytes):
+        self.id = user_id
         self.name = name
+        self.email = email
         self.password = password
 
-    __table_args__ = {'extend_existing': True}
+    id = db.Column(db.String(36), primary_key=True)
+    name = db.Column(db.String(50), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    password = db.Column(db.LargeBinary(128), nullable=False)
 
-    __tablename__ = 'user'
-    id: Mapped[str] = mapped_column(primary_key=True)
-    name: Mapped[str] = mapped_column()
-    email: Mapped[str] = mapped_column(unique=True)
-    password: Mapped[bytes] = mapped_column()
-    friends: Mapped[str] = mapped_column(default='')
+    friends = db.relationship('User', secondary=friendship,
+                              primaryjoin=id==friendship.c.user_id,
+                              secondaryjoin=id==friendship.c.friend_id,
+                              backref=db.backref('friend_of', lazy='dynamic'))
+
+    sent_requests = db.relationship('FriendshipRequest', backref='sender', lazy=True, foreign_keys='FriendshipRequest.sender_id')
+    received_requests = db.relationship('FriendshipRequest', backref='receiver', lazy=True, foreign_keys='FriendshipRequest.receiver_id')
+
+    chats = db.relationship('Chat', primaryjoin=id==chat_members.c.user_id, secondaryjoin=id==chat_members.c.chat_id,
+                            backref=db.backref('chat_of'),secondary=chat_members)
+
+    messages = db.relationship('Message', backref='user', lazy=True)
 
     def __repr__(self) -> str:
         return f"{{id:{self.id}, \
         name:{self.name}, \
         email: {self.email}, \
         password: {self.password}, \
-        friends:{self.friends}, \
-        }}"
+        friends:{self.friends}}}"
